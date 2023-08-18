1379358090cc6fe18297ddbf6b4436e4636e0d8c
Steven Glasford <stevenglasford@duck.com>
2023-08-17 22:30:43 -0500

    Add filtering for "zero" roads

--- find_grid_intersections.sql
+++ find_grid_intersections.sql
@@ -10,6 +10,8 @@ WITH RoadIntersections AS (
         a.highway IS NOT NULL AND
         b.highway IS NOT NULL AND
         a.osm_id < b.osm_id  -- Avoid duplicate intersections
+        ST_Distance(ST_StartPoint(a.way), ST_EndPoint(a.way)) > 0 AND  -- filter "zero" roads
+        ST_Distance(ST_StartPoint(b.way), ST_EndPoint(b.way)) > 0
 ),
 
 GridIntersections AS (
