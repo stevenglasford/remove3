7ac3df3873a9006c09b5a7b432a0b067eb1d3b37
Steven Glasford <stevenglasford@duck.com>
2023-08-21 17:27:41 -0500

    Create a new altering for the northbound streets

---
+++ alter_north_north_south.sql
@@ -0,0 +1,23 @@
+-- Start the transaction
+BEGIN;
+
+-- Update the planet_osm_line table based on the roads_info table for roads that are already North to South
+UPDATE planet_osm_line
+SET way = planet_osm_line.way,  -- Replace with any additional columns you may want to update
+    oneway = 'yes'  -- Mark as one-way
+FROM roads_info
+WHERE 
+    planet_osm_line.osm_id = roads_info.osm_id AND
+    ST_Y(ST_StartPoint(planet_osm_line.way)) < ST_Y(ST_EndPoint(planet_osm_line.way));
+
+-- Update the planet_osm_line table based on the roads_info table for roads that are South to North
+UPDATE planet_osm_line
+SET way = ST_Reverse(planet_osm_line.way),  -- Reverse the way geometry
+    oneway = 'yes'  -- Mark as one-way
+FROM roads_info
+WHERE 
+    planet_osm_line.osm_id = roads_info.osm_id AND
+    ST_Y(ST_StartPoint(planet_osm_line.way)) > ST_Y(ST_EndPoint(planet_osm_line.way));
+
+-- Commit the transaction
+COMMIT;
