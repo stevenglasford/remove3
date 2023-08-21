7c76638f5ba76cf2548be4f19b66885ac8d1c49d
Steven Glasford <stevenglasford@duck.com>
2023-08-20 20:04:41 -0500

    Alter lane selection for the eastwest roads

---
+++ select_north_east_west.sql
@@ -0,0 +1,24 @@
+WITH NumberedRows AS (
+    SELECT
+        osm_id,
+        min_latitude,
+        ROW_NUMBER() OVER (ORDER BY osm_id) AS rn,
+        LEAD(min_latitude, 1) OVER (ORDER BY osm_id) as next_lat
+    FROM
+        eastwestgrid AS SortedRoads
+),
+
+SelectArterials AS (
+    SELECT 
+        osm_id,
+        min_latitude
+    FROM
+        NumberedRows
+    WHERE
+        (rn % 6 = 1 OR rn % 6 = 4) OR min_latitude = next_lat
+)
+
+SELECT
+    osm_id
+FROM 
+    SelectArterials;
