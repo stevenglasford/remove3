df85134b67269bdb520068c581d1b55516afca9a
Steven Glasford <stevenglasford@duck.com>
2023-08-21 17:37:13 -0500

    Change the RoadInfo to the new items will test once back

--- alter_south_north_south.sql
+++ alter_south_north_south.sql
@@ -5,18 +5,18 @@ BEGIN;
 UPDATE planet_osm_line
 SET way = planet_osm_line.way,  -- Replace with any additional columns you may want to update
     oneway = 'yes'  -- Mark as one-way
-FROM roads_info
+FROM NorthSouthSouth
 WHERE 
-    planet_osm_line.osm_id = roads_info.osm_id AND
+    planet_osm_line.osm_id = NorthSouthSouth.osm_id AND
     ST_Y(ST_StartPoint(planet_osm_line.way)) > ST_Y(ST_EndPoint(planet_osm_line.way));
 
 -- Update the planet_osm_line table based on the roads_info table for roads that are South to North
 UPDATE planet_osm_line
 SET way = ST_Reverse(planet_osm_line.way),  -- Reverse the way geometry
     oneway = 'yes'  -- Mark as one-way
-FROM roads_info
+FROM NorthSouthSouth
 WHERE 
-    planet_osm_line.osm_id = roads_info.osm_id AND
+    planet_osm_line.osm_id = NorthSouthSouth.osm_id AND
     ST_Y(ST_StartPoint(planet_osm_line.way)) < ST_Y(ST_EndPoint(planet_osm_line.way));
 
 -- Commit the transaction
